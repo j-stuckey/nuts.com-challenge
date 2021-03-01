67b0b738adc2651cd62e1946c28a95a4ac67718f
James Stuckey <jstuckey@rutgers.edu>
2021-02-28 23:11:51 -0500

    Getting images

---
+++ next.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    images: {
+        domains: [
+            'nuts.com',
+            'rackcdn.com',
+            '1b0bbb9e89b4713adcc7-aea4cee2cb18344b328e3a03eff3ec4f.ssl.cf1.rackcdn.com'
+        ]
+    }
+};
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 import { getProductsData } from '../lib/products';
+import Image from 'next/image';
 
 export async function getStaticProps() {
     const products = await getProductsData();
@@ -22,16 +23,37 @@ export default function Home({ products }) {
             <main className={styles.main}>
                 <ul>
                     {products.map((product) => {
-
-                        console.log(product.masterData.current.masterVariant.prices[0].value.type);
+                        const { current } = product.masterData;
                         return (
                             <li key={product.id}>
-                                <p>{product.masterData.current.name.en}</p>
-                                <p>
-                                    {product.masterData.current.description.en}
-                                </p>
-
-
+                                <p>{current.name.en}</p>
+                                {current.masterVariant.images.length ? (
+                                    <Image
+                                        src={
+                                            current.masterVariant.images[0]
+                                                .url ||
+                                            'https://1b0bbb9e89b4713adcc7-aea4cee2cb18344b328e3a03eff3ec4f.ssl.cf1.rackcdn.com/ece4edb2868a8225.cro-U2aFaCJE.jpg'
+                                        }
+                                        height={150}
+                                        width={150}
+                                    />
+                                ) : (
+                                    <Image
+                                        src="https://1b0bbb9e89b4713adcc7-aea4cee2cb18344b328e3a03eff3ec4f.ssl.cf1.rackcdn.com/ece4edb2868a8225.cro-U2aFaCJE.jpg"
+                                        height={150}
+                                        width={150}
+                                    />
+                                )}
+                                {current.masterVariant.prices.length ? (
+                                    <p>
+                                        $
+                                        {current.masterVariant.prices[0].value
+                                            .centAmount / 100}
+                                    </p>
+                                ) : (
+                                    <p>'n/a'</p>
+                                )}
+                                <p>{current.description.en}</p>
                             </li>
                         );
                     })}
