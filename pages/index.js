e54f9aaa70c64372de57f2707d09db38e61b0086
James Stuckey <jstuckey@rutgers.edu>
2021-02-28 22:31:20 -0500

    Http module in progress

--- pages/index.js
+++ pages/index.js
@@ -3,20 +3,7 @@ import styles from '../styles/Home.module.css';
 import { getProductsData } from '../lib/products';
 
 export async function getStaticProps() {
-    // const allProductsData = getProductsData();
-
-    const response = await fetch(
-        'https://api.commercetools.co/nuts-custom-demo-1/products?offset=0&limit=10',
-        {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                Authorization: `Bearer ${process.env.ACCESS_TOKEN}`
-            }
-        }
-    );
-    const data = await response.json();
-    const products = data.results;
+    const products = await getProductsData();
 
     return {
         props: {
@@ -26,7 +13,6 @@ export async function getStaticProps() {
 }
 
 export default function Home({ products }) {
-    
     return (
         <div className={styles.container}>
             <Head>
@@ -36,12 +22,16 @@ export default function Home({ products }) {
             <main className={styles.main}>
                 <ul>
                     {products.map((product) => {
+
+                        console.log(product.masterData.current.masterVariant.prices[0].value.type);
                         return (
                             <li key={product.id}>
                                 <p>{product.masterData.current.name.en}</p>
                                 <p>
                                     {product.masterData.current.description.en}
                                 </p>
+
+
                             </li>
                         );
                     })}
