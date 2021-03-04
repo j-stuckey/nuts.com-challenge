12dbe54a7999e998c6fab60081849948e97e178b
James Stuckey <jstuckey@rutgers.edu>
2021-03-03 22:55:11 -0500

    Added image to modal

--- components/Modal.js
+++ components/Modal.js
@@ -1,3 +1,4 @@
+import Image from 'next/image';
 import styles from './Modal.module.css';
 
 export default function Modal({ isOpen, product, handleClose }) {
@@ -8,19 +9,45 @@ export default function Modal({ isOpen, product, handleClose }) {
     if (isOpen && product) {
         const isOrganic = product.masterData.current.masterVariant.attributes.find(
             (el) => el.name === 'Organic'
-        )
-
+        );
+        console.log(product);
         return (
             <div className={styles.modal}>
                 <section className={styles.modalMain}>
-                    <button type="button" onClick={handleClose}>
+                    <button
+                        type="button"
+                        onClick={handleClose}
+                        className={styles.close}
+                    >
                         X
                     </button>
-
-                    <h1>{product.masterData.current.name.en}</h1>
-                    {isOrganic && isOrganic.value ? (
-                        <img src="https://1b0bbb9e89b4713adcc7-aea4cee2cb18344b328e3a03eff3ec4f.ssl.cf1.rackcdn.com/5e85d71501308335-L2AE6hCf-thumb.jpg" />
-                    ) : null}
+                    <div className={styles.content}>
+                        <h1 className={styles.name}>
+                            {product.masterData.current.name.en}
+                        </h1>
+                        <div className={styles.images}>
+                            {isOrganic && isOrganic.value ? (
+                                <Image
+                                    src="https://1b0bbb9e89b4713adcc7-aea4cee2cb18344b328e3a03eff3ec4f.ssl.cf1.rackcdn.com/5e85d71501308335-L2AE6hCf-thumb.jpg"
+                                    layout="intrinsic"
+                                    width={40}
+                                    height={30}
+                                />
+                            ) : null}
+                            <Image
+                                src={
+                                    product.masterData.current.masterVariant
+                                        .images[0]
+                                        ? product.masterData.current
+                                              .masterVariant.images[0].url
+                                        : 'https://1b0bbb9e89b4713adcc7-aea4cee2cb18344b328e3a03eff3ec4f.ssl.cf1.rackcdn.com/ece4edb2868a8225.cro-U2aFaCJE.jpg'
+                                }
+                                width={220}
+                                height={300}
+                                layout="intrinsic"
+                            />
+                        </div>
+                    </div>
                 </section>
             </div>
         );
