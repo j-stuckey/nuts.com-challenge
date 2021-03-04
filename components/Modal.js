6920fc246c0ac35edcba6cf27bc4acc4f55091bc
James Stuckey <jstuckey@rutgers.edu>
2021-03-03 19:10:08 -0500

    Organic badge

--- components/Modal.js
+++ components/Modal.js
@@ -5,8 +5,11 @@ export default function Modal({ isOpen, product, handleClose }) {
         ? `${styles.modal} ${styles.block}`
         : `${styles.modal} ${styles.none}`;
 
-    const { current } = product.masterData;
-    if (isOpen) {
+    if (isOpen && product) {
+        const isOrganic = product.masterData.current.masterVariant.attributes.find(
+            (el) => el.name === 'Organic'
+        )
+
         return (
             <div className={styles.modal}>
                 <section className={styles.modalMain}>
@@ -14,7 +17,10 @@ export default function Modal({ isOpen, product, handleClose }) {
                         X
                     </button>
 
-                    <h1>{current.name.en}</h1>
+                    <h1>{product.masterData.current.name.en}</h1>
+                    {isOrganic && isOrganic.value ? (
+                        <img src="https://1b0bbb9e89b4713adcc7-aea4cee2cb18344b328e3a03eff3ec4f.ssl.cf1.rackcdn.com/5e85d71501308335-L2AE6hCf-thumb.jpg" />
+                    ) : null}
                 </section>
             </div>
         );
