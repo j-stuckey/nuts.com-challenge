a151a9936a06e57bb5946b006e3c9866ef46739c
James Stuckey <jstuckey@rutgers.edu>
2021-03-04 20:45:31 -0500

    Refactor Card component

--- components/Card.js
+++ components/Card.js
@@ -2,34 +2,18 @@ import styles from './Card.module.css';
 import Image from 'next/image';
 
 function Card({ id, name, images, prices, handleClose }) {
+  const backupImg =
+    'https://1b0bbb9e89b4713adcc7-aea4cee2cb18344b328e3a03eff3ec4f.ssl.cf1.rackcdn.com/ece4edb2868a8225.cro-U2aFaCJE.jpg';
 
+  const price = prices.length ? `$${prices[0].value.centAmount / 100}` : 'N/a';
+
+  const imgURL = images.length ? images[0].url : backupImg;
   return (
-    <li key={id}  className={styles.card} onClick={handleClose}>
+    <li key={id} className={styles.card} onClick={handleClose}>
       <p>{name}</p>
-          {images.length ? (
-            <Image
-              src={
-                images[0].url ||
-                'https://1b0bbb9e89b4713adcc7-aea4cee2cb18344b328e3a03eff3ec4f.ssl.cf1.rackcdn.com/ece4edb2868a8225.cro-U2aFaCJE.jpg'
-              }
-              height={150}
-              width={150}
-            />
-          ) : (
-            <Image
-              src="https://1b0bbb9e89b4713adcc7-aea4cee2cb18344b328e3a03eff3ec4f.ssl.cf1.rackcdn.com/ece4edb2868a8225.cro-U2aFaCJE.jpg"
-              height={150}
-              width={150}
-            />
-          )}
-          {prices.length ? (
-            <p>
-              $
-              {prices[0].value.centAmount / 100}
-            </p>
-          ) : (
-            <p>N/a</p>
-          )}
+      <Image src={imgURL} height={150} width={150} />
+
+      <p>{price}</p>
     </li>
   );
 }
